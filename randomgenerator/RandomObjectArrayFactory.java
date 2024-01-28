58a07336c663f0374ee4a69f28ec813cc6c10d08
Kaan YILMAZ <kaanyilmaz-2002@hotmail.com>
2024-01-29 01:07:19 +0300

    devices and randomgeneretor packages uptaded

---
+++ randomgenerator/RandomObjectArrayFactory.java
@@ -0,0 +1,59 @@
+package application.randomgenerator;
+
+import java.util.Random;
+import javaUtility.string.*;
+import javaUtility.datetime.*;
+import javaUtility.math.*;
+import javaUtility.array.*;
+
+public class RandomObjectArrayFactory {
+    private final Random m_random;
+
+    //String, Fraction, Date, int [], Point
+    private Object createObject()
+    {
+        Object object;
+
+        switch (m_random.nextInt(5)) {
+            case 0:
+                object = StringUtil.getRandomTextTR(m_random, m_random.nextInt(10) + 1);
+                break;
+            case 1:
+                object = new Fraction(m_random.nextInt(10) - 5, m_random.nextInt(10) + 1);
+                break;
+            case 2:
+                object = Date.random(m_random);
+                break;
+            case 3:
+                object = ArrayUtil.getRandomArray(m_random, m_random.nextInt(10) + 5, 0, 99);
+                break;
+            default:
+                object = Point.createCartesian(m_random.nextInt(200) - 100, m_random.nextInt(200) - 100);
+        }
+
+        return object;
+    }
+
+    private Object [] fillObjects(Object [] objects)
+    {
+        for (int i = 0; i < objects.length; ++i)
+            objects[i] = createObject();
+
+        return objects;
+    }
+
+    public RandomObjectArrayFactory()
+    {
+        this(new Random());
+    }
+
+    public RandomObjectArrayFactory(Random random)
+    {
+        m_random = random;
+    }
+
+    public Object[] getObjects(int count)
+    {
+        return fillObjects(new Object[count]);
+    }
+}
---
+++ randomgenerator/RandomObjectArrayGeneratorApp.java
@@ -0,0 +1,52 @@
+package application.randomgenerator;
+
+import java.util.Random;
+
+import javaUtility.datetime.*;
+import javaUtility.math.*;
+import javaUtility.array.*;
+import java.util.Scanner;
+
+public class RandomObjectArrayGeneratorApp {
+    private RandomObjectArrayGeneratorApp() {
+    }
+
+    public static void run() {
+        Random random = new Random();
+        RandomObjectArrayFactory factory = new RandomObjectArrayFactory(random);
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Dizinin eleman sayısını giriniz:");
+        int count = Integer.parseInt(kb.nextLine());
+
+        for (Object object : factory.getObjects(count)) {
+            System.out.println("--------------------------------------------------");
+            String typeName = object.getClass().getName();
+            System.out.printf("Dinamik tür ismi:%s%n", typeName);
+
+            if (object instanceof String) {
+                String s = (String) object;
+
+                System.out.println(s.toUpperCase());
+            } else if (object instanceof Fraction) {
+                Fraction f = (Fraction) object;
+
+                System.out.println(f.toString());
+            } else if (object instanceof Date) {
+                Date date = (Date) object;
+
+                System.out.println(date.toLongDateStringTR());
+            } else if (object instanceof int[]) {
+                int[] a = (int[]) object;
+
+                ArrayUtil.display(2, a);
+            } else {
+                Point p = (Point) object;
+
+                System.out.println(p.toString());
+                p.offset(20);
+                System.out.println(p.toString());
+            }
+            System.out.println("--------------------------------------------------");
+        }
+    }
+}
